b51c5696e8842032814a29471adb097b6a4db06d
high on tantor <josephvenable@verizon.net>
2026-04-22 21:18:24 -0400

    Update exepath.sh

--- exepath.sh
+++ exepath.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
+  clang++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -arch arm64 -arch x86_64; ./exepath;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+    g++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./exepath;
   else
-    clang++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall; ./a.out;
+    clang++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall; ./exepath;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -static; ./a.out;
+  clang++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -static; ./exepath;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./exepath;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./exepath;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -lkvm -static; ./a.out;
+  clang++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -lkvm -static; ./exepath;
 elif [ $(uname) = "SunOS" ]; then
-  g++ exepath/exepath.cpp exepath.cpp -o a.out -std=c++17 -Wall -static-libgcc; ./a.out;
+  g++ exepath/exepath.cpp exepath.cpp -o exepath -std=c++17 -Wall -static-libgcc; ./exepath;
 else
-  g++ exepath/exepath.cpp exepath.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./a.out.exe;
+  g++ exepath/exepath.cpp exepath.cpp -o exepath.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./exepath.exe;
 fi
