bf42663121d664b04350ac143fba27a7198dcfee
high on tantor <josephvenable@verizon.net>
2026-05-02 02:40:28 -0400

    Update internal.sh

--- internal.sh
+++ internal.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -arch arm64 -arch x86_64; ./__getexecname;
+  clang++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -arch arm64 -arch x86_64; ./internal;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
+    g++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
   else
-    clang++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall; ./__getexecname;
+    clang++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall; ./internal;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -static; ./__getexecname;
+  clang++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -static; ./internal;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
+  g++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
+  g++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -lkvm -static; ./__getexecname;
+  clang++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -lkvm -static; ./internal;
 elif [ $(uname) = "SunOS" ]; then
-  g++ __getexecname/internal.cpp internal.cpp -o __getexecname -std=c++17 -Wall -static-libgcc; ./__getexecname;
+  g++ __getexecname/internal.cpp internal.cpp -o internal -std=c++17 -Wall -static-libgcc; ./internal;
 else
-  g++ __getexecname/internal.cpp internal.cpp -o __getexecname.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./__getexecname.exe;
+  g++ __getexecname/internal.cpp internal.cpp -o internal.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./internal.exe;
 fi
